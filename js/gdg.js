fec96e0be78f32537c214e2d0ab7a0898a632bfc
Joshua Woodward <jw@woodwardmedia.net>
2013-02-09 22:04:36 -0800

    v 0.2 upload
    Added bootstrap-responsive.min.css
    
    Events
    - Categorized upcoming and past events,
    
    Photos
    - Changed layout, pulls last 22 photos

--- js/gdg.js
+++ js/gdg.js
@@ -1,5 +1,6 @@
 var Config = (function(){
     var config = {
+        //modify these
         'name' : _CHAPTER_NAME_,
         'id' : _CHAPTER_ID_,
         'api': _API_KEY_
@@ -7,23 +8,21 @@ var Config = (function(){
     return {get : function(a) { return config[a]}}
 })();
 
-
+$('title').prepend(Config.get('name')+' | ');
 $('.brand').html('<strong>'+Config.get('name')+'</strong>');
 $('li#googleplus').click(function(){window.open('https://plus.google.com/'+Config.get('id'))});
 
 //google+ page info
-$.get('https://www.googleapis.com/plus/v1/people/'+Config.get('id')+'?fields=aboutMe%2Ccover%2FcoverPhoto%2CdisplayName%2Cimage%2CplusOneCount&key='+Config.get('api'), function(data){
+$.get('https://www.googleapis.com/plus/v1/people/'+Config.get('id')+'?fields=aboutMe%2Ccover%2FcoverPhoto%2CdisplayName%2Cimage%2CplusOneCount&key='+Config.get('google_api'), function(data){
     $('#about').next().next().html(data.aboutMe);
 })
 
 
-var dates=[];
 //gdg dev site events feed
-var now = new Date();
-
 $.get("http://gdgfresno.com/gdgfeed.php?id="+Config.get('id'),function(data){
+    var now = new Date();
     for(var i=data.length-1;i>=0;i--){
-        var start = new Date(data[i].start);dates.push(start);
+        var start = new Date(data[i].start);
         
         var format = start.format("longDate")
         format += ' '+start.format("shortTime")
@@ -37,22 +36,38 @@ $.get("http://gdgfresno.com/gdgfeed.php?id="+Config.get('id'),function(data){
                         '</div>';        
         html +='</div>';
         
-		if (start < now){
-            $('#events').next().next().append(html);
-		} else {
-            $('#newevents').next().next().prepend(html);
-		}
+	if (start < now){
+            $('#past_events').next().next().append(html);
+	} else {
+            $('#upcoming_events').next().next().prepend(html);
+	}
     }
+    var past = $('#past_events').next().next().children();
+    if(past.length > 5 ){
+        $('#past_events').next().next().append('<div id="view_more_events"><a>More...</a></div>');
+    }
+    for( var i = past.length-1; i>=5; i--){
+        past[i].style.display='none';
+    }
+    $('#view_more_events').click(function(){
+        $('#past_events').next().next().children().slideDown();
+        this.style.display='none';
+    });
 },'json');
 
 //google+ photos
 var parsePWA = function(d){
-    var p = d.feed.entry;
+    var url, html, p = d.feed.entry, count=0;
     for(var x in p){
-        var url = p[x].content.src+'?sz=260';
-        var html = '<li class="span3"><a href="'+p[x].link[1].href+'" class="thumbnail" target="_blank"><img src="'+ url + '" alt="'+p[x].title.$t+'" title="'+p[x].summary.$t+'"></a></li>'
+        count++;
+        if(count == 1){
+            html = '<li class="span6"><a href="'+p[x].link[1].href+'" class="thumbnail" target="_blank"><img src="'+ p[x].content.src + '?sz=460" alt="'+p[x].title.$t+'" title="'+p[x].summary.$t+'"></a></li>'
+        }else if(count == 8){
+            html = '<li class="span6 pull-right"><a href="'+p[x].link[1].href+'" class="thumbnail" target="_blank"><img src="'+ p[x].content.src + '?sz=460" alt="'+p[x].title.$t+'" title="'+p[x].summary.$t+'"></a></li>'
+        }else{
+            html = '<li class="span3"><a href="'+p[x].link[1].href+'" class="thumbnail" target="_blank"><img src="'+ p[x].content.src + '?sz=260" alt="'+p[x].title.$t+'" title="'+p[x].summary.$t+'"></a></li>'
+        }
         $('#photo_container').append(html);
     }
 };
-
-$.get('https://picasaweb.google.com/data/feed/api/user/'+Config.get('id')+'/?alt=json-in-script&callback=parsePWA&max-results=24&kind=photo');
+$.get('https://picasaweb.google.com/data/feed/api/user/'+Config.get('id')+'/?alt=json-in-script&callback=parsePWA&max-results=22&kind=photo');
